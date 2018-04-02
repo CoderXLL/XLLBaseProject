7a627778d0ce557a9faadccca3063ab1971a3388
iOS-肖乐乐 <xiaoll@bbdtek.com>
2018-04-02 09:50:02 +0800

    add project

---
+++ XLLBaseProject/XLLBaseProject/Category/UIView+Quick.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Quick.m
+//  XLLBaseProject
+//
+//  Created by 肖乐 on 2018/3/30.
+//  Copyright © 2018年 iOSCoder. All rights reserved.
+//
+
+#import "UIView+Quick.h"
+
+@implementation UIView (Quick)
+
+@end
---
+++ XLLBaseProject/XLLBaseProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XLLBaseProject
+//
+//  Created by 肖乐 on 2018/3/30.
+//  Copyright © 2018年 iOSCoder. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
